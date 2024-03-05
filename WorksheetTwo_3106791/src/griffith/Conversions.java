59bbcfd6c0f33c7922a9770ed3d78ba42a310a36
Vlad <vlad.yurev.06@gmail.com>
2024-03-05 00:47:02 +0000

    Creataed blank methods

--- WorksheetTwo_3106791/src/griffith/Conversions.java
+++ WorksheetTwo_3106791/src/griffith/Conversions.java
@@ -8,12 +8,28 @@ package griffith;
  */
 public class Conversions {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	public double celsiusToFahrenheit(int celsius) {
+		return 0;
+		
+	}
+	public double fahrenheitToCelsius(int farhenheit) {
+		return 0;
+		
+	}
+	public double milesToKilometer(int miles) {
+		return 0;
+		
+	}
+	public double kilometerToMiles(int kilometer) {
+		return 0;
+		
+	}
+
+	
 }
